a37b42f802b02c38be39d068957c58f4a82cd197
Yan Chen <yan.chen@dfinity.org>
2022-09-09 14:32:16 -0700

    cow

--- src/utils.rs
+++ src/utils.rs
@@ -1,22 +1,23 @@
 use anyhow::anyhow;
 use candid::parser::value::IDLValue;
+use std::borrow::Cow;
 
-pub fn stringify(v: &IDLValue) -> anyhow::Result<String> {
+pub fn stringify(v: &IDLValue) -> anyhow::Result<Cow<str>> {
     Ok(match v {
-        IDLValue::Text(str) => str.to_string(),
-        IDLValue::Number(n) => n.to_string(),
-        IDLValue::Int64(n) => n.to_string(),
-        IDLValue::Int32(n) => n.to_string(),
-        IDLValue::Int16(n) => n.to_string(),
-        IDLValue::Int8(n) => n.to_string(),
-        IDLValue::Nat64(n) => n.to_string(),
-        IDLValue::Nat32(n) => n.to_string(),
-        IDLValue::Nat16(n) => n.to_string(),
-        IDLValue::Nat8(n) => n.to_string(),
-        IDLValue::Nat(n) => n.to_string(),
-        IDLValue::Int(n) => n.to_string(),
-        IDLValue::Principal(id) => id.to_string(),
-        IDLValue::Bool(b) => b.to_string(),
+        IDLValue::Text(str) => Cow::Borrowed(str),
+        IDLValue::Number(n) => Cow::Owned(n.to_string()),
+        IDLValue::Int64(n) => Cow::Owned(n.to_string()),
+        IDLValue::Int32(n) => Cow::Owned(n.to_string()),
+        IDLValue::Int16(n) => Cow::Owned(n.to_string()),
+        IDLValue::Int8(n) => Cow::Owned(n.to_string()),
+        IDLValue::Nat64(n) => Cow::Owned(n.to_string()),
+        IDLValue::Nat32(n) => Cow::Owned(n.to_string()),
+        IDLValue::Nat16(n) => Cow::Owned(n.to_string()),
+        IDLValue::Nat8(n) => Cow::Owned(n.to_string()),
+        IDLValue::Nat(n) => Cow::Owned(n.to_string()),
+        IDLValue::Int(n) => Cow::Owned(n.to_string()),
+        IDLValue::Principal(id) => Cow::Owned(id.to_string()),
+        IDLValue::Bool(b) => Cow::Owned(b.to_string()),
         _ => return Err(anyhow!("Cannot stringify {}", v)),
     })
 }
