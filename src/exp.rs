02363f0150e9826bb6689fead2b33c250c72ccdd
Yan Chen <yan.chen@dfinity.org>
2022-10-19 12:01:07 -0700

    refactor

--- src/exp.rs
+++ src/exp.rs
@@ -428,74 +428,77 @@ let _ = decode as "{canister}".{method} _.Ok.return;
 }
 
 pub fn project(helper: &MyHelper, value: IDLValue, path: &[Selector]) -> Result<IDLValue> {
-    if path.is_empty() {
-        return Ok(value);
-    }
-    let (head, tail) = (&path[0], &path[1..]);
-    match (value, head) {
-        (IDLValue::Opt(opt), Selector::Option) => project(helper, *opt, tail),
-        (IDLValue::Vec(mut vs), Selector::Index(idx)) => {
-            let idx = *idx as usize;
-            if idx < vs.len() {
-                project(helper, vs.swap_remove(idx), tail)
-            } else {
-                Err(anyhow!("{} out of bound {}", idx, vs.len()))
-            }
-        }
-        (IDLValue::Vec(vs), head @ (Selector::Map(func) | Selector::Filter(func))) => {
-            let mut new_helper = helper.spawn();
-            let mut res = Vec::with_capacity(vs.len());
-            for v in vs.into_iter() {
-                new_helper.env.0.insert(String::new(), v.clone());
-                let arg = Exp::Path(String::new(), Vec::new());
-                let exp = Exp::Apply(func.to_string(), vec![arg]);
-                match (head, exp.eval(&new_helper)) {
-                    (Selector::Map(_), v) => res.push(v?),
-                    (Selector::Filter(_), Ok(IDLValue::Bool(false))) => (),
-                    (Selector::Filter(_), Ok(_)) => res.push(v),
-                    (Selector::Filter(_), Err(_)) => (),
-                    (_, _) => unreachable!(),
+    let mut result = value;
+    for head in path.iter() {
+        match (result, head) {
+            (IDLValue::Opt(opt), Selector::Option) => result = *opt,
+            (IDLValue::Vec(mut vs), Selector::Index(idx)) => {
+                let idx = *idx as usize;
+                if idx < vs.len() {
+                    result = vs.swap_remove(idx);
+                } else {
+                    return Err(anyhow!("{} out of bound {}", idx, vs.len()));
                 }
             }
-            project(helper, IDLValue::Vec(res), tail)
-        }
-        (IDLValue::Vec(vs), Selector::Fold(init, func)) => {
-            let init = init.clone().eval(helper)?;
-            let mut new_helper = helper.spawn();
-            let mut acc = init;
-            for v in vs.into_iter() {
-                new_helper.env.0.insert(String::new(), v);
-                let arg = Exp::Path(String::new(), Vec::new());
-                new_helper.env.0.insert("_".to_string(), acc.clone());
-                let accu = Exp::Path("_".to_string(), Vec::new());
-                let exp = Exp::Apply(func.to_string(), vec![accu, arg]);
-                acc = exp.eval(&new_helper)?;
+            (IDLValue::Vec(vs), head @ (Selector::Map(func) | Selector::Filter(func))) => {
+                let mut new_helper = helper.spawn();
+                let mut res = Vec::with_capacity(vs.len());
+                for v in vs.into_iter() {
+                    new_helper.env.0.insert(String::new(), v.clone());
+                    let arg = Exp::Path(String::new(), Vec::new());
+                    let exp = Exp::Apply(func.to_string(), vec![arg]);
+                    match (head, exp.eval(&new_helper)) {
+                        (Selector::Map(_), v) => res.push(v?),
+                        (Selector::Filter(_), Ok(IDLValue::Bool(false))) => (),
+                        (Selector::Filter(_), Ok(_)) => res.push(v),
+                        (Selector::Filter(_), Err(_)) => (),
+                        (_, _) => unreachable!(),
+                    }
+                }
+                result = IDLValue::Vec(res);
             }
-            project(helper, acc, tail)
-        }
-        (IDLValue::Record(fs), field @ (Selector::Index(_) | Selector::Field(_))) => {
-            let id = field.to_label();
-            if let Some(v) = fs.into_iter().find(|f| f.id == id) {
-                return project(helper, v.val, tail);
+            (IDLValue::Vec(vs), Selector::Fold(init, func)) => {
+                let init = init.clone().eval(helper)?;
+                let mut new_helper = helper.spawn();
+                let mut acc = init;
+                for v in vs.into_iter() {
+                    new_helper.env.0.insert(String::new(), v);
+                    let arg = Exp::Path(String::new(), Vec::new());
+                    new_helper.env.0.insert("_".to_string(), acc.clone());
+                    let accu = Exp::Path("_".to_string(), Vec::new());
+                    let exp = Exp::Apply(func.to_string(), vec![accu, arg]);
+                    acc = exp.eval(&new_helper)?;
+                }
+                result = acc;
             }
-            Err(anyhow!("record field {:?} not found", field))
-        }
-        (
-            IDLValue::Variant(VariantValue(f, _)),
-            field @ (Selector::Index(_) | Selector::Field(_)),
-        ) => {
-            if field.to_label() == f.id {
-                project(helper, f.val, tail)
-            } else {
-                Err(anyhow!("variant field {:?} not found", field))
+            (IDLValue::Record(fs), field @ (Selector::Index(_) | Selector::Field(_))) => {
+                let id = field.to_label();
+                if let Some(v) = fs.into_iter().find(|f| f.id == id) {
+                    result = v.val;
+                } else {
+                    return Err(anyhow!("record field {:?} not found", field));
+                }
+            }
+            (
+                IDLValue::Variant(VariantValue(f, _)),
+                field @ (Selector::Index(_) | Selector::Field(_)),
+            ) => {
+                if field.to_label() == f.id {
+                    result = f.val;
+                } else {
+                    return Err(anyhow!("variant field {:?} not found", field));
+                }
+            }
+            (value, head) => {
+                return Err(anyhow!(
+                    "selector {:?} cannot be applied to {}",
+                    head,
+                    value
+                ))
             }
         }
-        (value, head) => Err(anyhow!(
-            "selector {:?} cannot be applied to {}",
-            head,
-            value
-        )),
     }
+    Ok(result)
 }
 
 impl std::str::FromStr for Exp {
